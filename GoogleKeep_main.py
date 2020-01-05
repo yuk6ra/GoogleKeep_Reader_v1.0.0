b641c791252d9a865e1541cc7e0d70b0e73fa6e1
Yuki Imamura <59524938+yuki-imamura-all@users.noreply.github.com>
2020-01-05 17:06:14 +0900

    Update GoogleKeep_main.py

--- GoogleKeep_main.py
+++ GoogleKeep_main.py
@@ -1,17 +1,19 @@
 import gkeepapi
 
-# Gmailアカウントでログイン
+# find Google Account ID and Password
 with open("Secret.txt", "r", encoding="utf-8") as Secret:
     reader = Secret.read().split()
-    userID = reader[0]
-    Password = reader[1]
+    account_id = reader[0]
+    password = reader[1]
 
 keep = gkeepapi.Keep()
 
-s = keep.login(userID, Password)
+# pass login
+keep.login(account_id, password)
 
-# memoと書かれたKeepを探します
-gnotes = keep.find(query='memo')
+# find the keyword of query="Test"
+g_memo = keep.find(query='Test')
 
-for x in gnotes:
+# indicate the found memos
+for x in g_memo:
     print(x.text + "/" + str(x.timestamps.created))
